27de3055f9db1bf9f0e3a2fc2605585de3fbddd6
alexbai94 <alexbai94@gmail.com>
2017-02-02 18:37:20 -0800

    Fixed issue 7

---
+++ application/controllers/Bingo.php
@@ -0,0 +1,31 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Bingo extends Application
+{
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Homepage for our app
+	 */
+	public function index()
+	{
+		// this is the view we want shown
+		
+
+		// build the list of authors, to pass on to our view
+		$source = $this->quotes->get(5);
+
+		$this->data = array_merge($this->data, $source);
+		$this->data['pagebody'] = 'justone';
+		$this->render();
+		
+	}
+	
+
+}
