f85c1c4de6c6e08bf91dd00862ee4258659ce6f8
ktaaam <kevint4m@gmail.com>
2017-02-03 15:16:19 -0800

    Fixed issue #9

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,10 +49,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'Hogwarts';
 $route['comp\d+/wisdom'] = "wise/bingo";
-$route['404_override'] = 'Welcome/random';
+$route['404_override'] = 'Hogwarts/random';
 $route['translate_uri_dashes'] = FALSE;
+$route['lock/(:any)/(:any)'] = 'Hogwarts/shucks';
 $route['lock/(:any)/(:any)'] = 'Welcome/shucks';
 $route['sleep'] = 'First/zzz';
 $route['show/(:num)'] = 'First/gimme/$1';
--- application/controllers/Wise.php
+++ application/controllers/Wise.php
@@ -1,27 +1,27 @@
 +<?php
- +
- +defined('BASEPATH') OR exit('No direct script access allowed');
- +
- +
- +/*
- + * To change this license header, choose License Headers in Project Properties.
- + * To change this template file, choose Tools | Templates
- + * and open the template in the editor.
- + */
- +
- +/**
- + * Description of Wise
- + *
- + * @author Kevin
- + */
- +class Wise extends Application {
- +    //put your code here
- +    
- +    public function bingo()
- +	{      
- +            $record = $this->quotes->get('6');
- +            $this->data = array_merge($this->data, $record);
- +            $this->data['pagebody'] = 'justone';
- +            $this->render();
- +	}
- +}
+ 
+ defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+ 
+ /*
+  * To change this license header, choose License Headers in Project Properties.
+  * To change this template file, choose Tools | Templates
+  * and open the template in the editor.
+  */
+ 
+ /**
+  * Description of Wise
+  *
+  * @author Kevin
+  */
+ class Wise extends Application {
+     //put your code here
+     
+     public function bingo()
+ 	{      
+             $record = $this->quotes->get('6');
+             $this->data = array_merge($this->data, $record);
+             $this->data['pagebody'] = 'justone';
+             $this->render();
+ 	}
+ }
